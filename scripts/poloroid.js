61614f692ce7855682e75e810b89f61a27d5aa53
Laura Gong <lg443@cornell.edu>
2025-02-14 18:22:35 -0500

    gallery mobile media query

---
+++ scripts/poloroid.js
@@ -0,0 +1,13 @@
+// $(document).ready(function(){
+//     $(".stubbycontainer").click(function() {
+//         $(this).css("animation", "stubby 6s");
+//         $(this).delay(6000).queue(function (next) {$(this).css("animation", "none"); next();});
+//     })
+// });
+
+
+// .poloroidcollage .poloroid:hover {
+//     transform: translate(0, -9px);
+//     box-shadow: 1px 1px 5px rgb(117, 67, 75);
+//     background-color: rgb(246, 193, 193)
+// }
