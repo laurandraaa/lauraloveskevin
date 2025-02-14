1ac7c19f282618c0e7ee040e4a1b06574cfa5b51
Laura Gong <lg443@cornell.edu>
2025-02-14 18:32:03 -0500

    poloroid hover to js file

--- scripts/poloroid.js
+++ scripts/poloroid.js
@@ -1,13 +1,11 @@
-// $(document).ready(function(){
-//     $(".stubbycontainer").click(function() {
-//         $(this).css("animation", "stubby 6s");
-//         $(this).delay(6000).queue(function (next) {$(this).css("animation", "none"); next();});
-//     })
-// });
-
-
-// .poloroidcollage .poloroid:hover {
-//     transform: translate(0, -9px);
-//     box-shadow: 1px 1px 5px rgb(117, 67, 75);
-//     background-color: rgb(246, 193, 193)
-// }
+$(document).ready(function(){
+    $(".poloroidcollage > .poloroid").hover(function() {
+        $(this).css("transform", "translate(0, -9px");
+        $(this).css("box-shadow", "1px 1px 5px rgb(117, 67, 75)");
+        $(this).css("background-color", "rgb(246, 193, 193)");
+    }, function () {
+        $(this).css("transform", "translate(0, 0");
+        $(this).css("box-shadow", "none");
+        $(this).css("background-color", "rgb(243, 210, 216)");
+    })
+});
