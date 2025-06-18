c0d3d428685eb847f287d6cb32f86dc49404b1fa
Laura Gong <lg443@cornell.edu>
2025-06-18 16:02:17 -0700

    better bucketlist styling

--- scripts/bucketlist.js
+++ scripts/bucketlist.js
@@ -1,15 +1,29 @@
 $(document).ready(function(){
     $("#inttrip").hover(function() {
-        $(this).css("font-size", "xx-large");
-        $(this).css("box-shadow", "1px 1px 5px rgb(117, 67, 75)");
+        $(this).css("background-color", "rgb(249, 244, 191)");
+        $(this).css("font-weight", "bold");
         $("#inttrip1").removeClass("hidden");
         $("#inttrip2").removeClass("hidden");
         $("#text").addClass("hidden");
     }, function () {
-        $(this).css("font-size", "x-large");
-        $(this).css("box-shadow", "none");
+        $(this).css("background-color", "transparent");
+        $(this).css("font-weight", "200");
         $("#inttrip1").addClass("hidden");
         $("#inttrip2").addClass("hidden");
         $("#text").removeClass("hidden");
     })
+    $("#lauragrad").hover(function() {
+        $(this).css("background-color", "rgb(249, 244, 191)");
+        $(this).css("font-weight", "bold");
+        $("#lauragrad1").removeClass("hidden");
+        $("#lauragrad2").removeClass("hidden");
+        $("#text").addClass("hidden");
+    }, function () {
+        $(this).css("background-color", "transparent");
+        $(this).css("font-weight", "200");
+        $("#lauragrad1").addClass("hidden");
+        $("#lauragrad2").addClass("hidden");
+        $("#text").removeClass("hidden");
+    })
+
 });
